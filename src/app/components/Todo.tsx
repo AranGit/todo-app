e75eedc2a5b60649e7ff3d9f9cb534ab6de0a81e
Phop <aran.kochhiran@gmail.com>
2024-02-22 18:09:06 +0700

    Fix Todo's layout

--- src/app/components/Todo.tsx
+++ src/app/components/Todo.tsx
@@ -57,7 +57,7 @@ function Todo(
 
   return (
     <div className={`todo-item p-2 ${className}`}>
-      <div className="flex flex-row flex-wrap items-center gap-2 w-full min-w-[200px]">
+      <div className="flex flex-row items-center gap-2 w-full min-w-[200px]">
         <input
           className="w-full h-full text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
           placeholder="Enter todo title"
@@ -65,9 +65,7 @@ function Todo(
           onChange={(e) => setCurrentTodo({ ...currentTodo, title: e.target.value })}
           disabled={currentAction === TodoAction.Idle || currentAction === TodoAction.Delete}
         />
-        <div className="flex flex-row flex-wrap items-center justify-end gap-2 w-full">
-          {buttons()}
-        </div>
+        {buttons()}
       </div>
 
     </div>
