1805e40ad60e5456bffccad907476caf37b6b1c5
Phop <aran.kochhiran@gmail.com>
2024-02-22 17:54:25 +0700

    Edit padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import TodosContainer from "./containers/TodosContainer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center p-4">
       <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
         <h2 className={`mb-3 text-2xl font-semibold text-center`}>
           TODO APP
