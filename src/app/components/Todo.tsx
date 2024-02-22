e55bd9f7ef58ab40bd340aa7ea4c23eb43ef9ac0
Phop <aran.kochhiran@gmail.com>
2024-02-22 17:54:15 +0700

    Create a TodoHandleButtons component for grouping edit and delete button

--- src/app/components/Todo.tsx
+++ src/app/components/Todo.tsx
@@ -4,6 +4,7 @@ import { TodoAction, TodoData } from '../data/todo'
 import PenIcon from '../Icons/PenIcon'
 import TrashIcon from '../Icons/TrashIcon'
 import ConfirmationButtons from './ConfirmationButtons'
+import TodoHandleButtons from './TodoHandleButtons'
 
 function Todo(
   { todo, className, action, onAction, onClose }:
@@ -47,22 +48,10 @@ function Todo(
           onConfirm={() => onUpdateTodo()}
         />
       default:
-        return <>
-          <button
-            className="select-none rounded-lg border bg-gray-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            type="button"
-            onClick={() => setCurrentAction(TodoAction.Edit)}
-          >
-            <PenIcon />
-          </button>
-          <button
-            className="flex justify-center items-center select-none rounded-lg bg-red-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            type="button"
-            onClick={() => onAction(currentTodo, TodoAction.Delete)}
-          >
-            <TrashIcon />
-          </button>
-        </>;
+        return <TodoHandleButtons
+          onEdit={() => setCurrentAction(TodoAction.Edit)}
+          onDelete={() => onAction(currentTodo, TodoAction.Delete)}
+        />
     }
   }
 
